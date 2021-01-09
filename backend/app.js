16f8a1f379038ff118341c914936447f0e341093
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2021-01-09 22:37:50 +0530

    basic structure of app.js of backend will done

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const MONGODB_URI = "";
 
 const app = express();
 
-app.listen(8000, ()=> {
-  console.log('app is running on port 8000');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+		next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is working!!')
 })
+
+app.use((error, req, res, next) => {
+	const status = error.statusCode || 500;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({
+		message: message,
+		data: data
+	});
+})
+
+const port = process.env.PORT || '8000';
+
+mongoose
+	.connect(MONGODB_URI)
+	.then(result => {
+		server.listen(port);
+	})
+	.catch(err => console.log(err));
