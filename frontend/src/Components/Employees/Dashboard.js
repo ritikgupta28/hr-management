559bfd659f637a7131f62cc2275939d4b30da23d
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2021-01-09 14:15:33 +0530

    adding folder and files

---
+++ frontend/src/Components/Employees/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <p>Email: test@test.com</p>
+      <p>Password: test</p>
+      <p>Phone No: +91 XXXXXXXXXX</p>
+      <p>Team Name: String</p>
+      <p>Team Members: abc, xyz, asd</p>
+    </div>
+  )
+}
+
+export default Dashboard
