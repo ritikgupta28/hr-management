e6b91739664df99638281f9173be46dc21da202c
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2021-01-09 19:12:52 +0530

    add

--- frontend/src/Components/Manager/Navbar.js
+++ frontend/src/Components/Manager/Navbar.js
@@ -6,42 +6,42 @@ import Notification from "./Notification"
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <Router >
-      <h1>HR-MANAGER</h1>
-      <div className="navbar_link">
-          <button><Link to="/">Dashboard</Link></button>
-          <button><Link to="/employee">Employee</Link></button>
-          <button><Link to="/notification">Notification</Link></button>
+    <Router >
+      <div className="navbar">
+       <h1>HR-MANAGER</h1>
+        <div className="navbar_link">
+         <button><Link to="/">Dashboard</Link></button>
+         <button><Link to="/employee">Employee</Link></button>
+         <button><Link to="/notification">Notification</Link></button>
+        </div>
+        <h2>MENU</h2>
       </div>
-      <h2>MENU</h2>
-      <div>
+      <div className="navbar_page">
         <Switch>
-        <Route
-          path='/'
+          <Route
+            path='/'
             exact
             render={props => (
-					<Dashboard />
-				)} 
-        />
-        <Route
-          path='/employee'
+					    <Dashboard />
+				    )} 
+          />
+          <Route
+            path='/employee'
             exact
             render={props => (
-            <EmployeeList />
-				)} 
-        />
-        <Route
-          path='/notification'
+              <EmployeeList />
+				    )} 
+          />
+          <Route
+            path='/notification'
             exact
             render={props => (
-					<Notification />
-				)} 
-        />
-      </Switch>
+					  <Notification />
+				    )} 
+          />
+        </Switch>
       </div>
-      </Router>
-      </div>
+    </Router>
   )
 }
 
