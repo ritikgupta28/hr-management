edcac657ec431b023205a8b571095f3134cc962f
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2021-01-11 01:43:48 +0530

    adding color in calender

--- frontend/src/Components/Manager/Notification.js
+++ frontend/src/Components/Manager/Notification.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import "./Notification.css"
 
 function Notification() {
-  const [value, onChange] = useState(new Date());
+  const [date, onDateChange] = useState(new Date());
 
+  const mark = [
+    '04-01-2021',
+  ]
+  const mark1 = [
+    '03-01-2021',
+    '05-01-2021'
+  ]
+  
   return (
     <div>
       <Calendar
-        onChange={onChange}
-        value={value}
+        onChange={onDateChange}
+        value={date}
+        tileClassName={({ date, view }) => {
+          if (mark.find(x => x === moment(date).format("DD-MM-YYYY"))) {
+            return 'highlight'
+          }
+          if (mark1.find(x => x === moment(date).format("DD-MM-YYYY"))) {
+            return 'highlight1'
+          }
+        }
+        }
       />
     </div>
   );
