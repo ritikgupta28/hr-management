c29625a44656c262384834a41c0fd3f022b0dd78
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2021-01-17 15:15:29 +0530

    adding login and signup

--- frontend/src/Components/Auth/Login.js
+++ frontend/src/Components/Auth/Login.js
@@ -2,13 +2,58 @@ import React, { useState } from 'react'
 import GoogleLogin from "react-google-login";
 import { actionType } from "../../reducer";
 import { useStateValue } from "../../StateProvider"
-import "./Auth.css"
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function Login() {
 
   const [{}, dispatch] = useStateValue();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const Copyright = () => {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="http://localhost:3000">
+          HR-MANAGER
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
   const onLogin = async (resData) => {
       dispatch({
@@ -92,38 +137,76 @@ function Login() {
   }
 
   const responseErrorGoogle = (response) => {
+    alert("Somthing went Wrong")
   }
 
+  const classes = useStyles();
+
   return (
-    <div className="login">
-      <p className="heading">LOGIN</p>
-      <div>
-      <div className="login_input">
-        <p className="input_lable">Email: </p>
-        <input
-          type="email"
-          placeholder="username@gmail.com"
-          value={email}
-          onChange = {e => setEmail(e.target.value)}  
-        />
-      </div>
-      <div className="login_input">
-        <p className="input_lable">Password: </p>
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange = {e => setPassword(e.target.value)}  
-        />
-      </div>
-      <div className="login_button">
-        <button
-          type="submit"
-          onClick={onSimpleLogin}
-        >
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
           Login
-        </button>
-        </div>
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={onSimpleLogin}
+            className={classes.submit}
+          >
+            Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+        <br />
         <GoogleLogin
           clientId="915015918185-g4cj40r77jv1cuvklra75hlc79kcmn41.apps.googleusercontent.com"
           buttonText="Login with Google"
@@ -132,7 +215,10 @@ function Login() {
           cookiePolicy={'single_host_origin'}
         />
       </div>
-    </div>
+      <Box mt={8}>
+        <Copyright />
+        </Box>
+    </Container>
   )
 }
 
--- frontend/src/Components/Auth/SignUp.js
+++ frontend/src/Components/Auth/SignUp.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
-import { actionType } from "../../reducer";
+import { actionType } from "../../reducer"
 import { useStateValue } from "../../StateProvider"
+import GoogleLogin from "react-google-login";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function SignUp() {
 
@@ -9,6 +21,41 @@ function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [mobile, setMobile] = useState("");
+
+  const Copyright = () => {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="http://localhost:3000">
+          HR-MANAGER
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  const classes = useStyles();
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -40,54 +87,138 @@ function SignUp() {
     };
   }
 
+  const responseSuccessGoogle = async (response) => {
+    try {
+        const res = await fetch('http://localhost:8000/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          tokenId: response.tokenId
+        })
+      })
+      const status = await res.status;
+      const resData = await res.json();
+      if (status === 500) {
+        throw new Error(resData.message);
+      }
+      setName("");
+      setEmail("");
+      setPassword("");
+      setMobile("");
+      alert("Done!");
+    } catch(err) {
+      dispatch({
+        type: actionType.SET_IS_AUTH,
+        isAuth: false
+      })
+      alert(err);
+    }
+  }
+
+  const responseErrorGoogle = (response) => {
+    alert("Somthing went Wrong")
+  }
+  
+
   return (
-    <div>
-      <p>SignUp</p>
-      <div className="">
-        <p>Name: </p>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange = {e => setName(e.target.value)}  
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form className={classes.form} noValidate>
+        <TextField
+            value={name}
+            onChange={e => setName(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+          />
+          <TextField
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            value={mobile}
+            onChange={e => setMobile(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="mobile"
+            label="Mobile"
+            name="mobile"
+            autoComplete="mobile"
+            autoFocus
+          />
+          <TextField
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={onSignUp}
+            className={classes.submit}
+          >
+            SignUp
+          </Button>
+          <Grid container>
+          <Grid item xs>
+              
+            </Grid>
+            <Grid item>
+              <Link href="/login" variant="body2">
+                {"You already have an account? Login"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+        <br />
+        <GoogleLogin
+          clientId="915015918185-g4cj40r77jv1cuvklra75hlc79kcmn41.apps.googleusercontent.com"
+          buttonText="SignUp with Google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={'single_host_origin'}
         />
       </div>
-      <div className="">
-        <p>Email: </p>
-        <input
-          type="email"
-          placeholder="username@gmail.com"
-          value={email}
-          onChange = {e => setEmail(e.target.value)}  
-        />
-      </div>
-      <div className="">
-        <p>Password: </p>
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange = {e => setPassword(e.target.value)}  
-        />
-      </div>
-      <div className="">
-        <p>Mobile: </p>
-        <input
-          type="text"
-          placeholder="Mobile"
-          value={mobile}
-          onChange = {e => setMobile(e.target.value)}  
-        />
-      </div>
-      <div className="">
-        <button
-          type="submit"
-          onClick={onSignUp}
-        >
-          SignUp
-        </button>
-      </div>
-    </div>
+      <Box mt={8}>
+        <Copyright />
+        </Box>
+      </Container>
   )
 }
 
