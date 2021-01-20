8d63d697192be1436156108eafdefe2fefbfeb1b
Ritik Gupta <ritikgupta2003@gmail.com>
2021-01-20 15:05:44 +0530

    landing page

--- frontend/src/Components/LandingPage/LandingPage.js
+++ frontend/src/Components/LandingPage/LandingPage.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+	Card,
+	CardContent
+} from '@material-ui/core/';
+
+import VerticalStepper from './Stepper';
+
+const useStylesCard = makeStyles({
+  root: {
+    width: 500,
+    margin: '10px',
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  conatiner: {
+  	display: 'flex',
+  },
+});
 
 function LandingPage() {
+  const classesCard = useStylesCard();
+
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  )
+  	<div className={classesCard.conatiner}>
+	  	<Card className={classesCard.root}>
+  			<CardContent>
+			    <div>
+		  	  	<p>For manager:</p>
+  		  	  <VerticalStepper />
+  			  </div>
+  			</CardContent>
+	  	</Card>
+  		<Card className={classesCard.root}>
+  			<CardContent>
+		  	  <div>
+		    		<p>For employee:</p>
+  		    	<VerticalStepper />
+	  		  </div>
+  			</CardContent>
+  		</Card>
+  	</div>
+  );
 }
 
 export default LandingPage;
